bdedbc15736ad6ef64fcaeffecc2427181d2fbb9
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2022-12-08 19:03:14 +0100

    Add working snipet

--- src/main/scala/IssueTapSink.scala
+++ src/main/scala/IssueTapSink.scala
@@ -19,3 +19,19 @@ object IssueTapSink extends ZIOAppDefault {
   override def run = program.repeatWhile(_ == 100000)
 
 }
+
+object SinkOK extends ZIOAppDefault {
+
+  val program = for {
+    ref <- Ref.make(0)
+    fib <- (ZStream
+      .range(0, 100000)
+      >>> ZSink.foreach(i => ref.update(_ + 1))).fork
+    _ <- fib.join
+    count <- ref.get
+    _ <- Console.printLine(s"count: $count")
+  } yield count
+
+  override def run = program.repeatWhile(_ == 100000)
+
+}
